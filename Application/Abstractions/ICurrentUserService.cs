9ecd36e5ca5759cfddf05db12b421a65f7ab0ced
Ferum2704 <protsysyn.bohdan@gmail.com>
2024-01-19 09:38:30 +0200

    Add current user service

---
+++ Application/Abstractions/ICurrentUserService.cs
@@ -0,0 +1,9 @@
+﻿using Application.Identitity;
+
+namespace Application.Abstractions
+{
+    public interface ICurrentUserService
+    {
+        bool IsInRole(ApplicationUserRole applicationRole);
+    }
+}
---
+++ Application/Identitity/CurrentUserService.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Identitity
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public bool IsInRole(ApplicationUserRole applicationRole)
+        {
+            var user = httpContextAccessor.HttpContext.User;
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            return user.IsInRole(applicationRole.ToString());
+        }
+    }
+}
